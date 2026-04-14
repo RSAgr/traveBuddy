a46916c924e7ddb0963e06a398dcd99553b51083
Rishi <maharishi459@gmail.com>
2026-04-14 21:29:02 +0530

    Reduced price according to Algo value

--- backend/services/api_fetcher.py
+++ backend/services/api_fetcher.py
@@ -7,20 +7,20 @@ def fetch_prices(constraints):
         components.append({
             "type": "transport",
             "mode": "flight",
-            "price": random.randint(3000, 6000)
+            "price": random.randint(300, 600)
         })
 
     if "train" in constraints["transport_modes"]:
         components.append({
             "type": "transport",
             "mode": "train",
-            "price": random.randint(1000, 3000)
+            "price": random.randint(100, 300)
         })
 
     components.append({
         "type": "stay",
         "mode": "hotel",
-        "price": random.randint(2000, 5000)
+        "price": random.randint(200, 500)
     })
 
     return components
